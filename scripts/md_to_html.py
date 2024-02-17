d2cdfd3da7a2d0dd80426c1d219f0cfb22b64b37
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-02-17 17:31:34 +0100

    Update site construction

--- scripts/md_to_html.py
+++ scripts/md_to_html.py
@@ -4,11 +4,16 @@ import lxml.etree as ET
 import re
 import yaml
 from marko.ext.gfm import gfm
+from jinja2 import Environment, FileSystemLoader
+
 
 tei_ns = "https://tei-c.org/ns/1-0/"
 ns_decl = {"tei": tei_ns}
 
 
+
+def position_to_schema(pos):
+    pass
 
 def write_to_log(message):
     with open("logs/log.txt", "a") as log_file:
@@ -20,7 +25,13 @@ def write_tree(tree, path):
         debug.write(ET.tostring(tree).decode('utf8'))
 
 
-def xmlify(path):
+def write_file(string, path):
+    with open(path, "w") as out_file:
+        out_file.write(string)
+        
+
+
+def htmlify(path, surrounding_files):
     """
     This function takes a md file and returns a TEI-like document (not fully compliant though)
     :param path: path to the md file
@@ -32,41 +43,107 @@ def xmlify(path):
     with open(path, "r") as input_file:
         as_text = input_file.read()
 
-    filename = file.split("/")[-1].replace(".md", "")
+    filename = path.split("/")[-1].replace(".md", "")
 
     ## Metadata management; yaml-xml conversion
     metadata_as_yaml = as_text.split("---")[1]
     python_dict = yaml.load(metadata_as_yaml, Loader=yaml.SafeLoader)
-    metadata = ET.Element("metadata")
-    for key, values in python_dict.items():
-        new_element = ET.Element(key)
-        if type(values) is list:
-            print(values)
-            new_element.text = ",".join(values)
-        else:
-            new_element.text = str(values)
-        metadata.append(new_element)
-    metadata_as_xml = ET.tostring(metadata, pretty_print=True, encoding='utf8').decode()
     # We remove the metadata from the text file
     transformed = as_text.replace(metadata_as_yaml, "")
+    print(transformed)
     ## Metadata management
 
     # GFM extension of marko parses tables too.
     converted_doc = gfm.convert(transformed)
+    converted_doc = converted_doc.replace("<hr />", "")
+    converted_doc = "<div>" + converted_doc + "</div>"
     parser = ET.HTMLParser(recover=True)
-    tree = ET.parse(converted_doc, parser=parser)
-    write_tree(tree, f"html/{filename}.html")
+    character_description = ET.fromstring(converted_doc)
+    character_description.set("class", "natural_language_desc")
+    yaml_data = python_dict
+    
+    # Next and previous files
+    previous_file, next_file = surrounding_files
+    next_and_previous = ET.Element("div")
+    print(surrounding_files)
+    if previous_file != None:
+        previous_file_path = previous_file.split("/")[-1].replace(".md", "")
+        out_previous_file_path = f"{previous_file_path}.html"
+        span = ET.Element("span")
+        span.set("class", "previous")
+        previous = ET.Element("a")
+        previous.set("href", out_previous_file_path)
+        previous.text = f"Previous character: {previous_file_path}"
+        next_and_previous.append(span)
+        span.append(previous)
+    if next_file != None:
+        next = ET.Element("a")
+        next_file_path = next_file.split("/")[-1].replace(".md", "")
+        out_next_file_path = f"{next_file_path}.html"
+        next.set("href", out_next_file_path)
+        next.text = f"Next character: {next_file_path}"
+        span = ET.Element("span")
+        span.set("class", "next")
+        next_and_previous.append(span)
+        span.append(next)
+    
+        
+    
 
+    # Load Jinja2 template from file
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('templates/char_template.html')
 
-    with open(f"../../data/to_tei/{dir_name}/{lesson_name}.xml", "w") as output_file:
-        # TODO: single asterisks are not rendered and break the HTML. After ET recovery, they are lost. I suppose the same
-        # TODO: happens with other chars. It's annoying.
-        # TODO: Another problem with regexp lesson: last urls disapear.
-        # TODO: linebreaks in code are removed by lxml. 
-        output_file.write(ET.tostring(lesson_as_xml_tree, pretty_print=True).decode('utf-8'))
+    # Render template with YAML data and save to HTML file
+    output = template.render(yaml_data)
+    character_table = ET.fromstring(output, parser=parser)
+    character_table_body = character_table.xpath("//body")
+    character_table_body[0].insert(2, character_description)
+    character_table_body[0].insert(-1, next_and_previous)
+    
+    with open(f"html/{filename}.html", 'w') as file:
+        file.write(ET.tostring(character_table, encoding='utf-8').decode())
 
 
 if __name__ == '__main__':
-    for file in glob.glob("data/characters/punctuations/*.md"):
+    pages = []
+    files = glob.glob("data/characters/punctuations/*.md")
+    files.sort(key=lambda x:x.split("/")[-1])
+    for index, file in enumerate(files):
+        filename = file.split("/")[-1].replace(".md", "")
+        classe = file.split("/")[-2]
         print(file)
-        xmlify(file)
+        try:
+            next_file = files[index + 1]
+        except IndexError:
+            next_file = None
+        
+
+        try:
+            previous_file = files[index - 1]
+        except IndexError:
+            previous_file = None
+        
+        htmlify(file, (next_file, previous_file))
+        pages.append((filename, classe))
+    
+    references = ""
+    references_global = ""
+    pages_as_dict = dict()
+    for page in pages:
+        nom_fichier, classe = page
+        try:
+            pages_as_dict[classe].append(nom_fichier)
+        except KeyError:
+            pages_as_dict[classe] = [nom_fichier]
+    print(pages_as_dict)
+    for classe, examples in pages_as_dict.items():
+        for page in pages:
+            nom_fichier, classe = page
+            references += f"<li><a href=\"html/{nom_fichier}.html\">{nom_fichier}</a></li>"
+        references_global += f"<li>{classe}<ul>{references}<ul></li>"
+    html_string = f"<!DOCTYPE html><html><body><ul>{references_global}<ul></body></html>"
+    with open("index.html", "w") as index:
+        index.write(html_string)
+        
+    
