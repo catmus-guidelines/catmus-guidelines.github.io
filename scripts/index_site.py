faf80de2790b52aac08e0abb1b14404fc73dcb30
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-02-26 13:51:15 +0100

    Add first index

--- scripts/index_site.py
+++ scripts/index_site.py
@@ -1,7 +1,26 @@
-
+import glob
+import uuid
+import re
+import lxml.etree as ET
 
 def index():
-    pass
+    index = list()
+    spaces_replacement = re.compile(r"\s+")
+    for html_page in glob.glob("html/guidelines/*/*.html"):
+        title = html_page.split("/")[-1].replace(".html", "")
+        parsed = ET.parse(html_page)
+        paragraphs = parsed.xpath("//p")
+        if len(paragraphs) > 0:
+            for paragraph in paragraphs:
+                par_dict = dict()
+                par_dict['id'] = str(uuid.uuid4())
+                par_dict['url'] = html_page
+                paragraph_text = re.sub(spaces_replacement, ' ', paragraph.text)
+                par_dict['par'] = paragraph_text
+                par_dict['title'] = title
+            index.append(par_dict)
+    print(index)
+        
 
 
 
